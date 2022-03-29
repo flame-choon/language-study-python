12cbda83391848fefd8ca70b9e062011aecd3e43
kwangjin.hwang <kwangjin.hwang@bespinglobal.com>
2022-03-29 13:51:25 +0900

    Update

--- Grammer/02_if_function.py
+++ Grammer/02_if_function.py
@@ -1,6 +1,8 @@
 
 # 조건문 , 함수
 
+# 조건문 
+# if, elif, else 로 원하는 분기처리를 한다
 if( 1 + 1 == 2):
     print("1 + 1은 2이다")
 
@@ -13,3 +15,18 @@ else:
     print("척은 미성년자입니다") 
 
 
+# 함수
+# 인자들을 받아서 결과 값을 도출
+def greet(name):
+    # f를 앞에 붙여주면 '{}' 내 값은 동일한 변수의 값으로 치환
+    print(f'안녕 {name}아~~, 잘 지내니?')
+
+greet('광진')    
+
+
+def greeting(name, location, age):
+    print(f"안녕 {name}아, 넌 {age} 살이고 {location}에 사는구나.")
+
+# 키워드 아규먼트 (kwarg)
+# 함수 생성 시, 의도된 파라미터의 타입에 맞게 입력을 전달하기 위해 사용
+greeting(name = "광진", age = 20, location = "서울")    
