cdace21554839516dba1f88927423f8dc918014b
kwangjin.hwang <kwangjin.hwang@bespinglobal.com>
2022-03-29 10:55:25 +0900

    Init

---
+++ Grammer/variable_type.py
@@ -0,0 +1,23 @@
+# 변수와 타입
+
+# 변수 
+# 어떤 값을 담고 있는 것. 이것을 변수
+# x, y가 변수.
+x = 1
+y = 'a'
+z = True
+
+print(x)
+
+# 타입
+# 각 값들이 가지고 있는 고유의 종류
+print(type(x))
+
+# python 에는
+a = 1 # int , 정수
+b = 1.0 # float , 소수
+c = 'a' # str, 문자열
+d = True #bool, 참/거짓
+
+print(a + b)
+
