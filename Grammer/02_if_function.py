9d24866ab3dbb9177705d58127c5791abd999540
kwangjin.hwang <kwangjin.hwang@bespinglobal.com>
2022-03-29 11:11:37 +0900

    Update

---
+++ Grammer/02_if_function.py
@@ -0,0 +1,15 @@
+
+# 조건문 , 함수
+
+if( 1 + 1 == 2):
+    print("1 + 1은 2이다")
+
+chuck_age = 19
+if(chuck_age >= 30):
+    print("척은 아재입니다")
+elif (chuck_age >= 19):
+    print("척은 성인입니다")
+else:
+    print("척은 미성년자입니다") 
+
+
