2881a63bb32330c12a791a6c11073c32ed5e0cb4
gwangjin-home <gwangjinhome@gwangjin-homes-MacBook-Pro.local>
2022-03-29 22:04:47 +0900

    Update

--- Grammer/ch06_package_module.py
+++ Grammer/ch06_package_module.py
@@ -1,12 +1,14 @@
 # 모듈 , 패키지
 
-import ch06_sms     # 모듈 이름 = 파일 이름
-import ch06_email
+# import <패키지 이름>.<모듈 이름>
+import ch06_package.ch06_sms as ch06_sms     
+import ch06_package.ch06_email as ch06_email
 
 s = ch06_sms.SMS()
 e = ch06_email.Email()
 
 s.send("010-1111-1111", "010-2222-2222", "BYE!!")
+s.ping("010-3333-3333")
 e.send("sweetmandoo@kakao.com", "chemi0110@gmail.com", "HI!!")
 
 
--- Grammer/ch06_sms.py
+++
@@ -1,3 +0,0 @@
-class SMS:
-    def send(self, from_number, to_number, body):
-        print(from_number + " 에서 " + to_number + " 로 문자를 보냈습니다: " + body)
