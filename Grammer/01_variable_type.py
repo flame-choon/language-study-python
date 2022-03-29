be99a0099b204bc412facf412eb9ee300dcf3ff9
kwangjin.hwang <kwangjin.hwang@bespinglobal.com>
2022-03-29 11:01:44 +0900

    update

--- Grammer/01_variable_type.py
+++ Grammer/01_variable_type.py
@@ -13,11 +13,18 @@ print(x)
 # 각 값들이 가지고 있는 고유의 종류
 print(type(x))
 
-# python 에는
+# python 에는 아래의 타입들로 구성
 a = 1 # int , 정수
 b = 1.0 # float , 소수
 c = 'a' # str, 문자열
 d = True #bool, 참/거짓
+
+# 타입 캐스팅
+# 변수가 가지고 있는 타입을 다른 타입으로 변화
+# 원하는 타입 함수를 호출하여 캐스팅
+print(str(c))
+
+
 
 print(a + b)
 
